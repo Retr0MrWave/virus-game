b6658d3cb76fa2bfc058c4a4c2f2f84845ab6679
Ilia Zhuravok <ilia.zhuravok@gmail.com>
2021-12-08 04:39:03 +0200

    Change setup.py to gui version

--- gui.py
+++ gui.py
@@ -26,7 +26,7 @@ P2CT = (0, 255//2, 0)
 # 1st parameter is the font file
 # which is present in pygame.
 # 2nd parameter is size of the font
-font = pygame.font.Font('freesansbold.ttf', 32)
+font = pygame.font.SysFont(None, 32)
 
 import VirusGame as vg
 
